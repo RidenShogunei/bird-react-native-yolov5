ef48073747bbad599a145070a669791e14b0e2b9
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2024-04-29 21:59:27 +0800

    还在奋斗视频,,,,

--- birdsearch/pages/video.tsx
+++ birdsearch/pages/video.tsx
@@ -1,60 +1,41 @@
-import React, {useState} from 'react';
-import {
-  Button,
-  View,
-  Alert,
-  Text,
-  ActivityIndicator,
-} from 'react-native';
-import RNFetchBlob from 'rn-fetch-blob'
-import Video from 'react-native-video';  // 引入Video组件
-import MediaControls, { PLAYER_STATES } from "react-native-media-controls";
+import Video from 'react-native-video';
+import {View, Button, ActivityIndicator, Keyboard, Text} from 'react-native';
+import fs from 'react-native-fs';
+import {video} from '../api/data';
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
+  const [videoUrl, setVideoUrl] = useState('');
   const [isLoading, setLoading] = useState(false);
-  const [label, setLabel] = useState('');
-  const [speed, setSpeed] = useState('');
-  const [videoUrl, setVideoUrl] = useState(null);
-
   const handleUploadImage = async () => {
     try {
       setLoading(true);
-      const result = await RNFetchBlob.fetch('POST', '/video', { 'Content-Type' : 'multipart/form-data' }, formdata);
-
-      if (result) {
-        let base64Str = result.data;
-        let binary = atob(base64Str.replace(/\s/g, ''));
-        let array = [];
-        for (let i = 0; i < binary.length; i++) {
-          array.push(binary.charCodeAt(i));
-        }
-        let blob = new Blob([new Uint8Array(array)], {type: 'video/mp4'});
-        let url = URL.createObjectURL(blob);
-        setVideoUrl(url);
-      }
-    } catch (err) {
-      console.error(err);
+      console.log('start send');
+      const Data = await video();
+      setVideoUrl(Data)
+    } catch (error) {
+      console.log('Unable to fetch/send video:', error);
     } finally {
       setLoading(false);
     }
   };
+ useEffect(() => {
+    console.log('video change', videoUrl);
+  }, [videoUrl]);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      {isLoading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        <>
-          {videoUrl && (
-                 <Video 
-                    source={{uri: videoUrl}}
-                    style={{width: 300, height: 200}}
-                    controls={true}
-                    resizeMode={"cover"}
-                />
-            )}
-          <Button title="上传视频" onPress={handleUploadImage} />
-        </>
+    <View>
+      <Button title="上传视频" onPress={handleUploadImage} />
+      {isLoading && <ActivityIndicator size="large" />}
+      {videoUrl && (
+        <View>
+          <Video
+            source={{uri: videoUrl}} // 使用videoData作为视频数据源
+            controls={true} // 显示控制条
+            repeat={true} // 重复播放
+          />
+          <Text>收到</Text>
+        </View>
       )}
     </View>
   );
